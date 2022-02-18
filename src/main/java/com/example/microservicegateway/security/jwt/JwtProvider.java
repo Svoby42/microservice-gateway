00fa9ba70bbea099692fec3ea069702f548ded34
svoby42 <qwerta009@gmail.com>
2022-02-18 01:23:05 +0100

    filtering

---
+++ src/main/java/com/example/microservicegateway/security/jwt/JwtProvider.java
@@ -0,0 +1,14 @@
+package com.example.microservicegateway.security.jwt;
+
+import com.example.microservicegateway.security.UserPrincipal;
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface JwtProvider {
+    String generateToken(UserPrincipal auth);
+
+    Authentication getAuthentication(HttpServletRequest request);
+
+    boolean isTokenValid(HttpServletRequest request);
+}
